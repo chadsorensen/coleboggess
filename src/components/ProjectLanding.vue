1b2518e07571f2d4944312ff337f713d35864b1f
chadsorensen <chdsorensen@gmail.com>
2018-04-13 11:28:19 -0700

    added reel, cover and autoplay to homepage

--- src/components/ProjectLanding.vue
+++ src/components/ProjectLanding.vue
@@ -1,8 +1,14 @@
 <template>
   <main>
-<!--     <video :poster="poster" controls>
-      <source :src="reel" type="video/mp4">
-    </video> -->
+    <div class="video-container" :style="{ backgroundImage: `url( ${poster} )`}" v-on:click.once="playVideo()">
+      <div class="play-button-holder" v-show="!showVideo">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-173 156 70 70" enable-background="new -173 156 70 70"><path class="svg-play" d="M-103 191l-70 35v-70z"/></svg>
+      </div>
+<!--       <video :poster="poster" controls class="reel cover" v-show="showVideo">
+        <source :src="reel" type="video/mp4">
+      </video> -->
+      <iframe v-if="showVideo" src="https://player.vimeo.com/video/67796616?autoplay=1&color=FFFFFF&title=0&byline=0&portrait=0" class="reel cover" width="1400" height="788" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    </div>
     <div id="show-projects" class="content-container">
       <app-projectList :limit="100"></app-projectList>
     </div>
@@ -13,19 +19,22 @@
 
 import ProjectList from './ProjectList'
 var reel = require('../assets/media/cole-boggess_reel_2013_HB.mp4');
-console.log("reel", reel)
 var poster = require('../assets/img/poster.jpg');
 export default {
   data () {
     return {
       reel,
-      poster
+      poster,
+      showVideo: false,
     }
   },
   components: {
     'app-projectList': ProjectList
   },
   methods: {
+    playVideo() {
+      this.showVideo = true;
+    }
   }
 }
 </script>
